92fecad54f7fffaa1c41970bc8de540c73909cfd
benbaessler <mail@benbaessler.com>
2022-05-03 18:11:35 +0100

    Added URI to test collateral

--- contracts/TestCollateral.sol
+++ contracts/TestCollateral.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Collateral is ERC721 {
+contract Collateral is ERC721URIStorage {
 
   constructor() ERC721("Collateral", "CLT") {}
 
   function mint(uint256 _id) public {
     _safeMint(msg.sender, _id);
+    _setTokenURI(_id, "ipfs://QmZ8JQYzNAVfSUsqXsouj4rhT11bRKGrpxWBgrK1K9yuFr");
   }
 
 }
