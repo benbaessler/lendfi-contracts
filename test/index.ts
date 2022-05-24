121b569b4c3082e5ab0ffc4253f3cfd66ea137a2
benbaessler <mail@benbaessler.com>
2022-05-24 04:55:03 +0100

    Updated tests

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("LoanFactory contract", () => {
+describe("LendFi contract", () => {
 
   let contract: any
   let collateral: any
@@ -12,25 +12,36 @@ describe("LoanFactory contract", () => {
   beforeEach(async () => {
     [user1, user2, user3] = await ethers.getSigners()
 
-    const Loan = await ethers.getContractFactory('LoanFactory');
-    contract = await Loan.deploy()
+    // Initializing LendFi contract
+    const LendFi = await ethers.getContractFactory('LendFi');
+    contract = await LendFi.deploy()
 
+    // Initializing Test Collateral NFT (ERC-721)
     const Collateral = await ethers.getContractFactory('Collateral') 
-    collateral = await Collateral.deploy()
-
+    collateral = await Collateral.deploy() 
+    
+    // Submitting a test loan
     await contract.submitLoan(
       user1.address, 
       user2.address, 
       ethers.utils.parseEther('.5'),
       ethers.utils.parseEther('.05'),
       { contractAddress: collateral.address, tokenId: 1 },
-      1658434728
+      Math.floor(Date.now() / 1000 + 1000)
     )
   })
 
-  describe('Initiating a loan', () => {
-
+  describe('Initializing a loan', () => {
     it('Should be able to submit a loan', async () => {
+      await contract.submitLoan(
+        user1.address, 
+        user2.address, 
+        ethers.utils.parseEther('.5'),
+        ethers.utils.parseEther('.05'),
+        { contractAddress: collateral.address, tokenId: 1 },
+        Math.floor(Date.now() / 1000 + 1000)
+      )
+
       const loan = await contract.getLoan(0)
       
       expect(loan.lender).to.equal(user1.address)
@@ -38,17 +49,71 @@ describe("LoanFactory contract", () => {
       expect(loan.amount).to.equal(ethers.utils.parseEther('.5'))
     })
 
+    it('Should not be able to submit a loan with a deadline in the past', async () => {
+      await expect(contract.submitLoan(
+        user1.address, 
+        user2.address, 
+        ethers.utils.parseEther('.5'),
+        ethers.utils.parseEther('.05'),
+        { contractAddress: collateral.address, tokenId: 1 },
+        Math.floor(Date.now() / 1000 - 1000)
+      )).to.be.revertedWith('Deadline can not be in the past')
+    })
+
+    it('Should not be able to submit a loan if the lender has insufficient funds', async () => {
+      await contract.submitLoan(
+        user1.address, 
+        user2.address, 
+        ethers.utils.parseEther('.5'),
+        ethers.utils.parseEther('.05'),
+        { contractAddress: collateral.address, tokenId: 1 },
+        Math.floor(Date.now() / 1000 + 1000)
+      )
+    })
+
+    it('Should not be able to submit a loan if both parties are the same', async () => {
+      await expect(contract.submitLoan(
+        user1.address, 
+        user1.address, 
+        ethers.utils.parseEther('.5'),
+        ethers.utils.parseEther('.05'),
+        { contractAddress: collateral.address, tokenId: 1 },
+        Math.floor(Date.now() / 1000 + 1000)
+      )).to.be.revertedWith('The lender and borrower can not be the same')
+    })
+  })
+
+  describe('Confirming a loan', async () => {
+    beforeEach(async () => {
+      await contract.submitLoan(
+        user1.address, 
+        user2.address, 
+        ethers.utils.parseEther('.5'),
+        ethers.utils.parseEther('.05'),
+        { contractAddress: collateral.address, tokenId: 1 },
+        Math.floor(Date.now() / 1000 + 1000)
+      )
+    })
+
     it('Should let lender confirm loan with loan deposit', async () => {
-      await contract.confirmLender(0, { value: ethers.utils.parseEther('.5') })
+      await contract.confirmLender(0, { value: ethers.utils.parseEther('.505') })
 
       const loan = await contract.getLoan(0)
       expect(loan.lenderConfirmed).to.equal(true)
     })
 
+    it('Should not let lender confirm loan if they do not send enough tokens', async () => {
+      await expect(
+        contract.confirmLender(0, { value: ethers.utils.parseEther('.25') })
+      ).to.be.revertedWith('Please send the amount you agreed to loaning out')
+    })
+
     it('Should let borrower confirm loan with collateral deposit', async () => {
+      // Minting test collateral NFT
       await collateral.connect(user2).mint(1)
 
-      // await collateral.connect(user2).setApprovalForAll(contract.address, true)
+      // Approving & confirming transfer of collateral
+      await collateral.connect(user2).setApprovalForAll(contract.address, true)
       await contract.connect(user2).confirmBorrower(0)
 
       const loan = await contract.getLoan(0)
@@ -58,9 +123,17 @@ describe("LoanFactory contract", () => {
       expect(collateralBalance).to.equal(1)
     })
 
+    it('Should revert borrower confirmation if collateral is not approved for the contract', async () => {
+      await collateral.connect(user2).mint(1)
+
+      await expect(
+        contract.connect(user2).confirmBorrower(0)
+      ).to.be.revertedWith('Token is not approved for this contract')
+    })
+
     it('Should activate loan as both parties confirm loan', async () => {
       // Confirming lender
-      await contract.confirmLender(0, { value: ethers.utils.parseEther('.5') })
+      await contract.confirmLender(0, { value: ethers.utils.parseEther('.505') })
 
       // Confirming borrower
       await collateral.connect(user2).mint(1)
@@ -71,18 +144,29 @@ describe("LoanFactory contract", () => {
       expect(loan.active).to.equal(true)
     })
 
+    // it('Should transfer assets to borrower on activate loan', async () => {
+    //   const balanceBefore = await user2.getBalance()
+
+    //   await contract.confirmLender(0, { value: ethers.utils.parseEther('.505') })
+    //   await collateral.connect(user2).mint(1)
+    //   await collateral.connect(user2).setApprovalForAll(contract.address, true)
+    //   await contract.connect(user2).confirmBorrower(0)
+
+    //   const balanceAfter = await user2.getBalance()
+    //   expect(balanceAfter.sub(balanceBefore)).to.equal(ethers.utils.parseEther('.5'))
+    // })
+
     it('Should not let third party confirm loan', async () => {
       await expect(contract.connect(user3).confirmLender(0)).to.be.revertedWith('You are not the lender of this loan')
       await expect(contract.connect(user3).confirmBorrower(0)).to.be.revertedWith('You are not the borrower of this loan')
     })
-
   })
 
   describe('Paying back a loan', () => {
 
     beforeEach(async () => {
       // Confirming lender
-      await contract.confirmLender(0, { value: ethers.utils.parseEther('.5') })
+      await contract.confirmLender(0, { value: ethers.utils.parseEther('.505') })
 
       // Confirming borrower
       await collateral.connect(user2).mint(1)
@@ -92,12 +176,19 @@ describe("LoanFactory contract", () => {
 
     it('Should let borrower pay back loan', async () => {
       const balanceBefore = await user1.getBalance()
-      await contract.connect(user2).paybackLoan(0, { value: ethers.utils.parseEther('.55') })
+      await contract.connect(user2).paybackLoan(0, { value: ethers.utils.parseEther('.5525') })
 
-      const loan = await contract.getLoan(0)
       const balanceAfter = await user1.getBalance()
+      expect(balanceAfter).to.equal(balanceBefore.add(ethers.utils.parseEther('.5525')))
+    })
 
-      expect(balanceAfter).to.equal(balanceBefore.add(ethers.utils.parseEther('.55')))
+    it('Should execute loan when it is paid', async () => {
+      await contract.connect(user2).paybackLoan(0, { value: ethers.utils.parseEther('.5525') })
+      const loan = await contract.getLoan(0)
+
+      expect(loan.active).to.equal(false)
+      expect(loan.executed).to.equal(true)
+      expect(loan.loanPaid).to.equal(true)
     })
     
     it('Should throw error if borrower does not provide enough value', async () => {
@@ -106,7 +197,7 @@ describe("LoanFactory contract", () => {
     })
 
     it('Should return collateral to borrower after payback', async () => {
-      await contract.connect(user2).paybackLoan(0, { value: ethers.utils.parseEther('.55') })
+      await contract.connect(user2).paybackLoan(0, { value: ethers.utils.parseEther('.5525') })
 
       const userCollateralBalance = await collateral.balanceOf(user2.address)
       const contractCollateralBalance = await collateral.balanceOf(contract.address)
@@ -116,7 +207,7 @@ describe("LoanFactory contract", () => {
     })
 
     it('Should execute loan after payback', async () => {
-      await contract.connect(user2).paybackLoan(0, { value: ethers.utils.parseEther('.55') })
+      await contract.connect(user2).paybackLoan(0, { value: ethers.utils.parseEther('.5525') })
 
       const loan = await contract.getLoan(0)
 
