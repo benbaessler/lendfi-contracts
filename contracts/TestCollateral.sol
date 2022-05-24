9192cd323f3bf929cac2dd3ec4862b054907cd1a
benbaessler <mail@benbaessler.com>
2022-05-24 04:56:02 +0100

    Tracking TestCollateral

---
+++ contracts/TestCollateral.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract Collateral is ERC721URIStorage {
+
+  constructor() ERC721("Collateral", "CLT") {}
+
+  function mint(uint256 _id) public {
+    _safeMint(msg.sender, _id);
+    _setTokenURI(_id, string(abi.encodePacked("ipfs://QmdRAvWJa2Ck3pQPVni1DhYHc1zZNvJnZWAacS3vfWuDYA/", _id)));
+  }
+
+}
